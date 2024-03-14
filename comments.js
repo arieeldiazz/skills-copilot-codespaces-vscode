aa6cb597d7f37005071ffd09375e3b0558f4dc6f
Ariel Díaz Z. <102308529+arieeldiazz@users.noreply.github.com>
2024-03-14 14:48:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,63 +1,23 @@
-// create web server
-
-// Require the http module
-var http = require('http');
+// Create web server
+var express = require('express');
+var app = express();
 var fs = require('fs');
-var url = require('url');
+var bodyParser = require('body-parser');
 var path = require('path');
-var qs = require('querystring');
 var comments = require('./comments.json');
+var cors = require('cors');
 
-// Create a server
-http.createServer(function (req, res) {
-  // Get the URL
-  var urlParts = url.parse(req.url);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // Check the URL
-  if(urlParts.pathname == '/') {
-    // Read the file
-    fs.readFile('index.html', function(err, data) {
-      // Write the file to response
-      res.end(data);
-    });
-  } else if(urlParts.pathname == '/comments') {
-    // Check the request method
-    if(req.method == 'POST') {
-      var body = '';
+// Get comments
+app.get('/api/comments', function(req, res) {
+  res.json(comments);
+});
 
-      // Read the data
-      req.on('data', function(data) {
-        body += data;
-      });
-
-      // When the request ends
-      req.on('end', function() {
-        // Parse the body
-        var POST = qs.parse(body);
-
-        // Add the comment
-        comments.push(POST.comment);
-
-        // Save the comments
-        fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
-          // Send the response
-          res.end('{"status": "success"}');
-        });
-      });
-    } else {
-      // Send the comments
-      res.end(JSON.stringify(comments));
-    }
-  } else {
-    // Get the file
-    var filePath = path.join(__dirname, urlParts.pathname);
-
-    // Read the file
-    fs.readFile(filePath, function(err, data) {
-      // Write the file to response
-      res.end(data);
-    });
-  }
-}).listen(3000);
-
-console.log('Server running at http://localhost:3000/');
+// Add comments
+app.post('/api/comments', function(req, res) {
+  var comment = {
+    id: Date.now(),
